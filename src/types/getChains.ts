9bd4c8b729bf133242884add244467097ea8e114
avirajkhare00 <avirajkhare00@gmail.com>
2025-06-25 14:11:53 +0530

    --amend

--- src/types/getChains.ts
+++ src/types/getChains.ts
@@ -15,25 +15,3 @@ export interface ChainResponse {
     }]
   }
 }
-
-/*
-{
-  "status": "success",
-  "data": {
-    "network": [
-      {
-        "caip_id": "eip155:56",
-        "network_name": "BSC",
-        "chain_id": "56",
-        "logo": "BSC",
-        "sponsorship_enabled": true,
-        "gsn_enabled": false,
-        "type": "EVM",
-        "network_id": "9a26afcf-ed62-43d8-91ea-42dc7cd55890",
-        "onramp_enabled": false,
-        "whitelisted": true
-      }
-    ]
-  }
-}
-*/
