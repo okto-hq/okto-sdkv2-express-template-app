103c366138e3c0da4d5144b62e95b2d3f0210aad
Ovia <151402233+oviawork@users.noreply.github.com>
2025-07-01 11:56:40 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,3 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const BASE_URL = process.env.BASE_URL || "https://sandbox-api.okto.tech";
-// export const CLIENT_SWA = process.env.OKTO_CLIENT_SWA as Hex;
-// export const OKTO_CLIENT_PRIVATE_KEY = process.env.OKTO_CLIENT_PRIVATE_KEY as string;
